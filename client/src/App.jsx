4458070733ca1825d028659351d77ed6fb217144
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-08-05 21:50:15 +0300

    Added the routes imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import Login from './pages/LogIn/LogIn';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LogIn from './pages/LogIn/LogIn';
 import SignUp from './pages/SignUp/SignUp';
 
 export default function App() {
@@ -8,24 +7,11 @@ export default function App() {
     <>
     <BrowserRouter>
     <Routes>
-        <Route index element={<Login/>} ></Route>
+        <Route index element={<LogIn/>} ></Route>
         <Route path="/SignUp" element={<SignUp/>} ></Route>
     </Routes>
     </BrowserRouter>
-=======
-import React from 'react'
-import './App.css'
-
-function App() {
-  
-  return (
-    <>
-      <div>
-        App
-      </div>
-
->>>>>>> c36b0e3a8a0db95fdab6094f58bb53e087931c99
-    </>
+   </>
 
   )
 }
