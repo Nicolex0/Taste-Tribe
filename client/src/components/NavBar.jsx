cc6fcec0011b21c4c3edcd21a3b4ccf4702782bd
Njagi Githaiga <njagigithaiga@gmail.com>
2024-08-07 03:22:01 +0300

    Adds NavBar and Footer Components

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  FaSearch,
+  FaBars,
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTiktok,
+} from "react-icons/fa";
+
+const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("");
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleNavItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <div>
+      <nav className="bg-customGreen text-white py-4 font-urbanist">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* Logo */}
+          <div className="flex items-center">
+            {/* <img src="../assets/T.T Logo.svg" alt="Taste Tribe" className="h-8 mr-2" /> */}
+            <span className="text-xl font-bold">Taste Tribe</span>
+          </div>
+
+          {/* Navigation Links */}
+          <div className="hidden md:flex space-x-6">
+            {["HOME", "RECIPES", "ABOUT US", "CONTACT US"].map((item) => (
+              <a
+                key={item}
+                href="#"
+                onClick={() => handleNavItemClick(item)}
+                className={`hover:text-gray-400 ${
+                  selectedItem === item ? "border-b-2 border-red-500" : ""
+                }`}
+              >
+                {item}
+              </a>
+            ))}
+          </div>
+
+          {/* Toggle Menu and Search Icon */}
+          <div className="flex space-x-4 md:hidden">
+            <button onClick={handleMenuToggle} className="hover:text-gray-400">
+              <FaBars size={24} />
+            </button>
+            <a href="#" className="hover:text-gray-400">
+              <FaSearch size={24} />
+            </a>
+          </div>
+
+          {/* Dropdown Menu for Mobile */}
+          {isMenuOpen && (
+            <div className="absolute top-16 left-0 w-full bg-customGreen md:hidden">
+              {["HOME", "RECIPES", "ABOUT US", "CONTACT US"].map((item) => (
+                <a
+                  key={item}
+                  href="#"
+                  onClick={() => handleNavItemClick(item)}
+                  className={`block py-2 px-4 hover:bg-gray-700 ${
+                    selectedItem === item ? "bg-gray-700" : ""
+                  }`}
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          )}
+
+          {/* Search Icon for larger screens */}
+          <div className="hidden md:flex space-x-4">
+            <a href="#" className="hover:text-gray-400">
+              <FaSearch size={24} />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
