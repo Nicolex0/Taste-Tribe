42174cb74ad96064f3c5754a34b3d2e01fab2fd5
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-08-08 13:37:15 +0300

    Created the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LogIn from './pages/LogIn/LogIn';
 import SignUp from './pages/SignUp/SignUp';
+import About from './pages/About/About'
+import ContactUs from './pages/ContactUs/ContactUs';
+import Recipes from './pages/Recipes/Recipes';
+import UserProfile from './pages/UserProfile/UserProfile'
+import Home from './pages/Home/Home'
 
 export default function App() {
   return (
     <>
     <BrowserRouter>
     <Routes>
-        <Route index element={<LogIn/>} ></Route>
-        <Route path="/SignUp" element={<SignUp/>} ></Route>
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/login" element={<LogIn/>} ></Route>
+        <Route path="/signUp" element={<SignUp/>} ></Route>
+        <Route path="/about" element={<About/>} ></Route>
+        <Route path="/contactUs" element={<ContactUs/>} ></Route>
+        <Route path="/recipes" element={<Recipes/>} ></Route>
+        <Route path="/userprofile" element={<UserProfile/>} ></Route>
     </Routes>
     </BrowserRouter>
    </>
