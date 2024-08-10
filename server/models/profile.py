44b8b7f9bb93bdb85f39562edb2b76379f0ab336
Xander-AJ <xander.kamau7@gmail.com>
2024-08-10 17:35:52 +0300

    implement profile model for user profiles

--- server/models/profile.py
+++ server/models/profile.py
@@ -5,4 +5,3 @@ class Profile(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     profile_image_url = db.Column(db.String(255))
     bio = db.Column(db.Text)
-
