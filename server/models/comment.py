7b63d8c153d44261d2a75a507973ce662f4058f8
Xander-AJ <xander.kamau7@gmail.com>
2024-08-10 17:34:27 +0300

    implement comment model for recipe comments

--- server/models/comment.py
+++ server/models/comment.py
@@ -8,4 +8,3 @@ class Comment(db.Model):
     recipe_id = db.Column(db.Integer, db.ForeignKey("recipe.id"), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
