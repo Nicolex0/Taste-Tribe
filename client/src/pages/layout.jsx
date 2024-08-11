585e6a4d9c07e5ed3ef02b0b9f3fb186e4c762a9
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-08-11 21:02:57 +0300

    Created a common/shared layout page to appear in all the pages

---
+++ client/src/pages/layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import { Outlet } from 'react-router-dom'
+const Layout = () => {
+  return (
+    <>
+    <NavBar/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+ 
+export default Layout
